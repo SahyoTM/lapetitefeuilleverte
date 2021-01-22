c07129c87d2f601ade8660a9c307435aa81cf6b7
SahyoTM <71664040+SahyoTM@users.noreply.github.com>
2021-01-22 13:39:34 +0100

    add : section info

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var animInput = lottie.loadAnimation({
+    container: document.getElementById('anim-input'), // récupérer l'ID de la div
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json' 
+  });
